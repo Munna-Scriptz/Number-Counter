d1485280c0cab20a42a6e37ad180630dcd730b85
Munna-Scripts <munnascripts@gmail.com>
2025-06-06 17:22:36 +0600

    Dom Selection Complete

---
+++ js/scritp.js
@@ -0,0 +1,37 @@
+// All Doms--------------------
+let TheBody     = document.querySelector('body')
+let AllMighty    = document.querySelector('.main')
+let CounterText  = document.querySelector('.Counter1')
+let CounterButton = document.querySelector('.Subscribe-btn')
+let CounterInput  = document.querySelector('.input')
+let ClearInputSvg = document.querySelectorAll('.icon')
+let InputError = document.querySelector('.Counter_error')
+let InputSpeed = document.querySelector('.SpeedInput')
+
+let i = 0
+let i2 = 0
+let i3 = 0
+let i4 = 0
+
+// Counter2--------------------
+let CounterText2  = document.querySelector('.Counter2')
+let CounterButton2 = document.querySelector('.Subscribe-btn2')
+let CounterInput2  = document.querySelector('.input2')
+
+// Counter3--------------------
+let CounterText3  = document.querySelector('.Counter3')
+let CounterButton3 = document.querySelector('.Subscribe-btn3')
+let CounterInput3  = document.querySelector('.input3')
+
+// Counter3--------------------
+let CounterText4  = document.querySelector('.Counter4')
+let CounterButton4 = document.querySelector('.Subscribe-btn4')
+let CounterInput4  = document.querySelector('.input4')
+
+
+
+
+// Counter Clear--------------------
+
+
+// Practice 15---------------------
