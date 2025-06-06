13f878ab4a7cde3b179d3d02ee718abaab06a6c2
Munna-Scripts <munnascriptz@gmail.com>
2025-06-06 18:03:59 +0600

    All Counter Complete

--- js/scritp.js
+++ js/scritp.js
@@ -25,13 +25,12 @@ let HandelButton1 = ()=>{
         i++
         CounterText.innerHTML = i
     
-        if( i == CounterInput.value){
+        if(i == CounterInput.value){
             clearInterval(StopInterval1)
         }
     }
 
     let StopInterval1 = setInterval(MyCounter1 , InputSpeed.value)
-    CounterInput.value = ''
 }
 // Counter2--------------------
 let CounterText2  = document.querySelector('.Counter2')
@@ -39,7 +38,25 @@ let CounterButton2 = document.querySelector('.Subscribe-btn2')
 let CounterInput2  = document.querySelector('.input2')
 
 
+let HandelButton2 = ()=>{
+    if(CounterInput2.value == ''){
+        AllMighty.style  = 'display:none;'
+        InputError.style = 'display:block;'
+        TheBody.style    = 'background: url(https://cdn.dribbble.com/userupload/23438801/file/original-524952a81a8523e762c8a8042afaba4e.gif);'
+    }
 
+    let MyCounter2 = ()=>{
+
+        i2++
+        CounterText2.innerHTML = i2
+    
+        if( i2 == CounterInput2.value){
+            clearInterval(StopInterval2)
+        }
+    }
+
+    let StopInterval2 = setInterval(MyCounter2 , InputSpeed.value)
+}
 
 // Counter3--------------------
 let CounterText3  = document.querySelector('.Counter3')
@@ -47,16 +64,50 @@ let CounterButton3 = document.querySelector('.Subscribe-btn3')
 let CounterInput3  = document.querySelector('.input3')
 
 
+let HandelButton3 = ()=>{
+    if(CounterInput3.value == ''){
+        AllMighty.style  = 'display:none;'
+        InputError.style = 'display:block;'
+        TheBody.style    = 'background: url(https://cdn.dribbble.com/userupload/23438801/file/original-524952a81a8523e762c8a8042afaba4e.gif);'
+    }
 
+    let MyCounter3 = ()=>{
 
+        i3++
+        CounterText3.innerHTML = i3
+    
+        if( i3 == CounterInput2.value){
+            clearInterval(StopInterval3)
+        }
+    }
 
+    let StopInterval3 = setInterval(MyCounter3 , InputSpeed.value)
+}
 
-// Counter3--------------------
+// Counter4--------------------
 let CounterText4  = document.querySelector('.Counter4')
 let CounterButton4 = document.querySelector('.Subscribe-btn4')
 let CounterInput4  = document.querySelector('.input4')
 
+let HandelButton4 = ()=>{
+    if(CounterInput4.value == ''){
+        AllMighty.style  = 'display:none;'
+        InputError.style = 'display:block;'
+        TheBody.style    = 'background: url(https://cdn.dribbble.com/userupload/23438801/file/original-524952a81a8523e762c8a8042afaba4e.gif);'
+    }
 
+    let MyCounter4 = ()=>{
+
+        i4++
+        CounterText4.innerHTML = i4
+    
+        if( i4 == CounterInput4.value){
+            clearInterval(StopInterval4)
+        }
+    }
+
+    let StopInterval4 = setInterval(MyCounter4 , InputSpeed.value)
+}
 
 
 // Counter Clear--------------------
